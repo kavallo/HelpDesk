cc4b135d4be372a180bd4ab220233315f1223b67
Rafael Alberto Gomez Urbaez <r.gomez5@unibe.edu.do>
2019-04-02 18:16:20 -0400

    Formulario DEPARTAMENTOS

---
+++ HelpDesk/Controllers/PersonasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelpDesk.Models;
+
+namespace HelpDesk.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private AppHelpDeskEntities db = new AppHelpDeskEntities();
+
+        // GET: Personas
+        public ActionResult Index()
+        {
+            var personas = db.Personas.Include(p => p.Departamento).Include(p => p.Estatu);
+            return View(personas.ToList());
+        }
+
+        // GET: Personas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartamentoID = new SelectList(db.Departamentos, "DepartamentoID", "Descripcion");
+            ViewBag.EstatusID = new SelectList(db.Estatus, "EstatusID", "Descripcion");
+            return View();
+        }
+
+        // POST: Personas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PersonaID,Codigo,Nombres,Apellidos,DepartamentoID,EsTecnico,EstatusID")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personas.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartamentoID = new SelectList(db.Departamentos, "DepartamentoID", "Descripcion", persona.DepartamentoID);
+            ViewBag.EstatusID = new SelectList(db.Estatus, "EstatusID", "Descripcion", persona.EstatusID);
+            return View(persona);
+        }
+
+        // GET: Personas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartamentoID = new SelectList(db.Departamentos, "DepartamentoID", "Descripcion", persona.DepartamentoID);
+            ViewBag.EstatusID = new SelectList(db.Estatus, "EstatusID", "Descripcion", persona.EstatusID);
+            return View(persona);
+        }
+
+        // POST: Personas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonaID,Codigo,Nombres,Apellidos,DepartamentoID,EsTecnico,EstatusID")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartamentoID = new SelectList(db.Departamentos, "DepartamentoID", "Descripcion", persona.DepartamentoID);
+            ViewBag.EstatusID = new SelectList(db.Estatus, "EstatusID", "Descripcion", persona.EstatusID);
+            return View(persona);
+        }
+
+        // GET: Personas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Persona persona = db.Personas.Find(id);
+            db.Personas.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
